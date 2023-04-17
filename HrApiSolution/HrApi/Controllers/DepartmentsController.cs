56bbee2ae98717c0a429ab46ce078d5f1daadcd0
lboone <lboone@progressive.com>
2023-04-17 16:33:03 -0400

    major db context changes.

--- HrApiSolution/HrApi/Controllers/DepartmentsController.cs
+++ HrApiSolution/HrApi/Controllers/DepartmentsController.cs
@@ -1,4 +1,6 @@
-﻿using HrApi.Domain;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using HrApi.Domain;
 using HrApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,9 +10,13 @@ namespace HrApi.Controllers;
 public class DepartmentsController : ControllerBase
 {
     private readonly HrDataContext _context;
-    public DepartmentsController(HrDataContext context)
+    private readonly IMapper _mapper;
+    private readonly MapperConfiguration _config;
+    public DepartmentsController(HrDataContext context, IMapper mapper, MapperConfiguration config)
     {
         _context = context;
+        _mapper = mapper;
+        _config = config;
     }
 
     [HttpPost("/departments")]
@@ -20,7 +26,24 @@ public class DepartmentsController : ControllerBase
         {
             return BadRequest();
         }
-        return Ok(request);
+
+
+
+        var departmentToAdd = _mapper.Map<DepartmentEntity>(request);
+
+        _context.Departments.Add(departmentToAdd);
+        try
+        {
+            await _context.SaveChangesAsync();
+
+            var response = _mapper.Map<DepartmentSummaryItem>(departmentToAdd);
+            return Ok(response);
+        }
+        catch (DbUpdateException)
+        {
+
+            return BadRequest("That Department Exists");
+        }
     }
 
     [HttpGet("/departments")]
@@ -28,8 +51,9 @@ public class DepartmentsController : ControllerBase
     {
         var response = new DepartmentsResponse
         {
-            Data = await _context.Departments.Select(d => 
-            new DepartmentSummaryItem {  Id = d.Id.ToString(), Name = d.Name }).ToListAsync()
+            Data = await _context.Departments
+                .ProjectTo<DepartmentSummaryItem>(_config)
+                .ToListAsync()
         };
         return Ok(response);
     }
