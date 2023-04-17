4158833fd0b356c5fac025de17c67cc74d88b263
lboone <lboone@progressive.com>
2023-04-17 14:36:40 -0400

    departments controller and http

--- HrApiSolution/HrApi/Controllers/DepartmentsController.cs
+++ HrApiSolution/HrApi/Controllers/DepartmentsController.cs
@@ -1,20 +1,24 @@
-﻿using HrApi.Models;
+﻿using HrApi.Domain;
+using HrApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HrApi.Controllers;
 
 public class DepartmentsController : ControllerBase
 {
+    private readonly HrDataContext _context;
+    public DepartmentsController(HrDataContext context)
+    {
+        _context = context;
+    }
     [HttpGet("/departments")]
     public async Task<ActionResult<DepartmentsResponse>> GetDepartments()
     {
         var response = new DepartmentsResponse
         {
-            Data = new List<DepartmentSummaryItem>
-            {
-                new DepartmentSummaryItem { Id="1", Name="Developers"},
-                new DepartmentSummaryItem { Id="2", Name="Testers"}
-            }
+            Data = await _context.Departments.Select(d => 
+            new DepartmentSummaryItem {  Id = d.Id.ToString(), Name = d.Name }).ToListAsync()
         };
         return Ok(response);
     }
