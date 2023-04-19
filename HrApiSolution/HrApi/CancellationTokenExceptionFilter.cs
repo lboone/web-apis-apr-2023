8078976328b79770c2612c7b90c31dca9cf93b89
lboone <lboone@progressive.com>
2023-04-19 12:33:34 -0400

    Jeffs Code before lunch 4_19_2023

--- HrApiSolution/HrApi/CancellationTokenExceptionFilter.cs
+++ HrApiSolution/HrApi/CancellationTokenExceptionFilter.cs
@@ -1,30 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
-namespace HrApi
+namespace HrApi;
+
+public class CancellationTokenExceptionFilter : IActionFilter, IOrderedFilter
 {
-    public class CancellationTokenExceptionFilter : IActionFilter, IOrderedFilter
+    public int Order => int.MaxValue - 10;
+
+    // This will run AFTER the controller returns a response.
+    public void OnActionExecuted(ActionExecutedContext context)
     {
-        public int Order => throw new NotImplementedException();
-
-        // This will run AFTER the controller returns a response.
-        public void OnActionExecuted(ActionExecutedContext context)
+        if (context.Exception is TaskCanceledException)
         {
-            if(context.Exception is TaskCanceledException)
+            Console.WriteLine("Got that cancellation");
+            context.Result = new ObjectResult(context.Exception.Message)
             {
-                Console.WriteLine("Got that cancellation");
-                context.Result = new ObjectResult(context.Exception.Message)
-                {
-                    StatusCode = 500
-                };
-                context.ExceptionHandled = true;
-            }
+                StatusCode = 500
+            };
+            context.ExceptionHandled = true;
         }
+    }
 
-        // This will run BEFORE the controller action is called.
-        public void OnActionExecuting(ActionExecutingContext context)
-        {
-            // Nothing to see here. Carry on.
-        }
+    // This will run BEFORE your controller action is called.
+    // "Fixing to execute your action"
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        // Nothing to see here. Carry on. 
     }
 }
--- HrApiSolution/HrApi/LoggingStuff.cs
+++ HrApiSolution/HrApi/LoggingStuff.cs
@@ -2,11 +2,11 @@
 
 public static class LoggingStuff
 {
-    public static async Task LogIt(HttpContext context,Func<Task> next)
+    public static async Task LogIt(HttpContext context, Func<Task> next)
     {
         await Console.Out.WriteLineAsync($"Just got a request from {context.Request.Headers.UserAgent}");
         await next();
-    }
+    } 
 
     public static IApplicationBuilder UseSuperLogging(this IApplicationBuilder app)
     {
