94bb42382ebac01d3997468cf02740a28f06c821
lboone <lboone@progressive.com>
2023-04-19 10:48:19 -0400

    middleware

---
+++ HrApiSolution/HrApi/CancellationTokenExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HrApi
+{
+    public class CancellationTokenExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order => throw new NotImplementedException();
+
+        // This will run AFTER the controller returns a response.
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if(context.Exception is TaskCanceledException)
+            {
+                Console.WriteLine("Got that cancellation");
+                context.Result = new ObjectResult(context.Exception.Message)
+                {
+                    StatusCode = 500
+                };
+                context.ExceptionHandled = true;
+            }
+        }
+
+        // This will run BEFORE the controller action is called.
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Nothing to see here. Carry on.
+        }
+    }
+}
